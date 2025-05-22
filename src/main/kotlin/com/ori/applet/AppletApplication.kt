6dcbd018f8ee73934a8b66dad74716e315523348
orihgc <1074464280@qq.com>
2025-05-22 23:11:42 +0800

    metric out put

--- src/main/kotlin/com/ori/applet/AppletApplication.kt
+++ src/main/kotlin/com/ori/applet/AppletApplication.kt
@@ -4,15 +4,21 @@ import com.ori.applet.aop.SpringHello
 import com.ori.applet.beans.Person
 import com.ori.applet.metric.SalesMetrics
 import com.ori.applet.properties.DataBaseProperties
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import sun.java2d.pipe.SpanShapeRenderer.Simple
 import kotlin.random.Random
 
 @SpringBootApplication
@@ -41,6 +47,14 @@ class AppletApplication {
     fun periodicallyMakeOrder() {
         val random = Random(0)
         salesMetrics?.makeNewOrder(amount = random.nextInt(100))
+    }
+
+    @Bean
+    fun customMeterRegistry(): MeterRegistry? {
+        val compositeMeterRegistry = CompositeMeterRegistry()
+        compositeMeterRegistry.add(SimpleMeterRegistry())
+        compositeMeterRegistry.add(LoggingMeterRegistry())
+        return compositeMeterRegistry
     }
 }
 
